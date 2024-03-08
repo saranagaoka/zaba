fe3aef5eb3116a2ca1a639cbbb8fb7613b73b439
Sara <sara.nagaoka@gmail.com>
2024-03-08 23:04:56 +0200

    zaba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [sometext, setSometext] = useState(false);
+  const napis_3 =
+    "Hi Konstantin! Wszystkiego najlepszego z okazji dnia mężczyzn!";
+
+  const foo = () => {
+    if (counter >= napis_3.length) {
+      return;
+    } else {
+      setCounter((prev) => prev + 1);
+      setTimeout(foo, 100);
+    }
+  };
+
+  const magicfoo = () => {
+    setSometext((prev) => !prev);
+  };
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    if (!sometext) {
+      foo();
+    } else {
+      setCounter(0);
+    }
+  }, [sometext]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {sometext ? (
+        <div className="cont">
+          <div className="napis2">{napis_3.slice(0, counter)}</div>
+        </div>
+      ) : (
+        <div className="napis">Pet the frog! </div>
+      )}
+
+      <div class="container" onClick={magicfoo}>
+        <div class="leg-l"></div>
+        <div class="leg-r"></div>
+        <div class="body"></div>
+        <div class="face">
+          <div class="eyes"></div>
+          <div class="tongue"></div>
+        </div>
+      </div>
     </div>
   );
 }
